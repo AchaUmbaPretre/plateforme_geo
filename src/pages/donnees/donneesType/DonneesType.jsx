24e2dc64f705a1f85fd6d88028cd8620ba009e3e
AchaUmbaPretre <[achandambi@gmail.com]>
2025-09-07 00:35:57 +0200

    commit

--- src/pages/donnees/donneesType/DonneesType.jsx
+++ src/pages/donnees/donneesType/DonneesType.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from 'react'
+import { getDonneesType } from '../../../services/donnees.service'
 import './donneesType.scss'
 
 const DonneesType = () => {
+    const [data, setData] = useState([]);
+
+    const fetchData = async() => {
+        try {
+            const { data } = await getDonneesType();
+            setData(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
 
   return (
     <>
