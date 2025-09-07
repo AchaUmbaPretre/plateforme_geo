0bd5139a5af5c4acd8a8e18470a36c2deec0a3bb
AchaUmbaPretre <[achandambi@gmail.com]>
2025-09-07 15:38:45 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react';
+import { lazy } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Layout from './components/layout/Layout';
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,10 +3,12 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../footer/Footer';
 import Navbar from '../navbar/Navbar';
 import './layout.scss'
+import ScrollToTop from '../scrollToTop/ScrollToTop';
 
 const Layout = () => (
   <div className='app-rows'>
     <Navbar />
+     <ScrollToTop /> 
     <div className="app-container">
       <Suspense fallback={<div className="loading">Chargement...</div>}>
         <Outlet />
